3cc5daac2687861f61b4eda7cbd3e6f0e073be04
Wesley  Souza <134818645+wesleyoliveire@users.noreply.github.com>
2024-08-23 22:38:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Header from './components/Header';
 import GameBanner from './components/GameBanner';
 import GameDescription from './components/GameDescription';
